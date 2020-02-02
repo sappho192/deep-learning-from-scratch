896c91aab98ff8e468e875e112e2de0cd2b032ba
TAE-IN Sappho KIM <sappho192@gmail.com>
2020-02-02 22:12:47 +0900

    Create Test code for NMatrix.Div() and Sub()

--- CSharp/DeepLearningLibrary/NumNetTest/NMatrixTest.cs
+++ CSharp/DeepLearningLibrary/NumNetTest/NMatrixTest.cs
@@ -26,8 +26,10 @@ namespace NumNetTest
                 new double[] { 6, 5, 4, 3, 2, 1 });
 
             AddTest(MatA, MatB, MatC);
+            SubTest(MatA, MatB, MatC);
             DotTest(MatA, MatB, MatC);
             MultTest();
+            DivTest();
         }
 
         private void AddTest(NMatrix MatA, NMatrix MatB, NMatrix MatC)
@@ -35,6 +37,14 @@ namespace NumNetTest
             var MatSum1 = MatA.Add(MatB);
             Assert.False(MatSum1.Invalid);
             var MatSum2 = MatA.Add(MatC);
+            Assert.True(MatSum2.Invalid);
+        }
+
+        private void SubTest(NMatrix MatA, NMatrix MatB, NMatrix MatC)
+        {
+            var MatSum1 = MatA.Sub(MatB);
+            Assert.False(MatSum1.Invalid);
+            var MatSum2 = MatA.Sub(MatC);
             Assert.True(MatSum2.Invalid);
         }
 
@@ -59,14 +69,31 @@ namespace NumNetTest
                 {
                     1,2,3,4,5,6
                 });
-            var MatMult1 = MatA.Mult(-0.5);
+            var MatMult = MatA.Mult(-0.5);
             var MatAnswer = new NMatrix(MatA.Row, MatA.Col,
                 new double[] {
                     -0.5, -1, -1.5,
                     -2, -2.5, -3
                 });
 
-            Assert.True(MatAnswer.Equals(MatMult1));
+            Assert.True(MatAnswer.Equals(MatMult));
+        }
+
+        private void DivTest()
+        {
+            var MatA = new NMatrix(2, 3,
+                new double[]
+                {
+                    1,2,3,4,5,6
+                });
+            var MatDiv = MatA.Div(-2);
+            var MatAnswer = new NMatrix(MatA.Row, MatA.Col,
+                new double[] {
+                    -0.5, -1, -1.5,
+                    -2, -2.5, -3
+                });
+
+            Assert.True(MatAnswer.Equals(MatDiv));
         }
     }
 }
