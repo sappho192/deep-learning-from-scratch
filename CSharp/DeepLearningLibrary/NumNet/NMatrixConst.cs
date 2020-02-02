a8f8ffaba92435ff90dfa61de429be5370daf69b
TAE-IN Sappho KIM <sappho192@gmail.com>
2020-02-02 19:43:21 +0900

    Create constant - NMatrix.Empty

---
+++ CSharp/DeepLearningLibrary/NumNet/NMatrixConst.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NumNet
+{
+    public partial class NMatrix
+    {
+        public static readonly NMatrix Empty = new NMatrix(0, 0, null);
+    }
+}
