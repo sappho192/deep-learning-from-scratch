a3bdb1e6ae782474dea58e4351b130559dacefe1
TAE-IN Sappho KIM <sappho192@gmail.com>
2020-02-02 17:57:09 +0900

    Added NMath Class to NumNet

--- CSharp/DeepLearningLibrary/NumNet/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace NumNet
-{
-    public class Class1
-    {
-    }
-}
---
+++ CSharp/DeepLearningLibrary/NumNet/NMath.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace NumNet
+{
+    public static class NMath
+    {
+        public static double Perceptron(
+            double w1, double w2, double theta,
+            double x1, double x2)
+        {
+            return w1 * x1 + w2 * x2 <= theta ? 0 : 1;
+        }
+
+        public static double AND(double x1, double x2)
+        {
+            return Perceptron(0.5, 0.5, 0.6, x1, x2);
+        }
+
+        public static double OR(double x1, double x2)
+        {
+            return Perceptron(0.5, 0.5, 0.4, x1, x2);
+        }
+
+        public static double NAND(double x1, double x2)
+        {
+            return Perceptron(-0.5, -0.5, -0.6, x1, x2);
+        }
+    }
+}
