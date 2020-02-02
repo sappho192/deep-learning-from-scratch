3344bc7a60ce4c41bcb8ac6f76476a50850ff89e
TAE-IN Sappho KIM <sappho192@gmail.com>
2020-02-02 20:18:45 +0900

    Create NMatrixTest.cs

---
+++ CSharp/DeepLearningLibrary/NumNetTest/NMatrixTest.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+using NumNet;
+
+namespace NumNetTest
+{
+    public class NMatrixTest
+    {
+        [Fact]
+        public void MatrixCreationTest()
+        {
+            Assert.True((new NMatrix(0, 1, null)).Invalid);
+            Assert.True((new NMatrix(1, 0, null)).Invalid);
+            Assert.True((new NMatrix(1, 1, null)).Invalid);
+            Assert.False(
+                (new NMatrix(1, 1, new double[] { 1.0 })).Invalid);
+        }
+    }
+}
